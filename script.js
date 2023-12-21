6324eabbc636c916e59ff453d1fae38d1c95c1b2
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-21 21:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //your JS code here. If required.
-function getSize() {
-	var w = document.documentElement.clientWidth;
-	var h = document.documentElement.clientHeight;
-	document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
-}
+  let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+
+    h.innerText="Width: "+width+" and Height: "+height; 
+
+  })
