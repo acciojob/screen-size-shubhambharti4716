c2f7f85b44b5212e046588768f37d91e0a07ef32
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-21 21:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 //your JS code here. If required.
-// const sizeElement = document.getElementById("sizeInfo");
-
-
-// document.body.append(sizeElement);
-//  window.onresize = function(){
-// 	 sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
-//  }
 function getSize() {
-			var w = document.documentElement.clientWidth;
-			var h = document.documentElement.clientHeight;
-			document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
-		}
+	var w = document.documentElement.clientWidth;
+	var h = document.documentElement.clientHeight;
+	document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
+}
